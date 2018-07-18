6ecbe6179b679c20f447349a8dabb6b030c69f7b
Wang, Yusheng <yusheng.wang@dtms.de>
2018-07-18 22:31:07 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ys_base',
+      version='0.1',
+      description='YS Base',
+      url='https://github.com/yush3n9/ys_base',
+      author='ys',
+      author_email='ys@ys.com',
+      license='MIT',
+      packages=['ys_base'],
+      zip_safe=False)
